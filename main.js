88cfaf8f02b4736b8d0381520a54aa099e397d1e
Alejandro Granadillo <slayerfat@gmail.com>
2016-06-30 21:10:17 -0400

    updated user time argument

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const CONSTANTS = require('./src/constants');
 // if the user sets a custom time.
 let time = process.argv.slice(2);
 time = time.length ? parseInt(time[0]) : CONSTANTS.HALF;
-let end = time < CONSTANTS.HOUR * 24 ? time : time * CONSTANTS.MIN;
+let end = time * CONSTANTS.MIN < CONSTANTS.HOUR * 24 ? time * CONSTANTS.MIN : time;
 
 const bar = new ProgressBar('[:bar] :percent :msg', {total: end, width: 40});
 const sound = new Sound({command: 'paplay'});
