ac7d3f82201e0d4bb6236af694c9f1ad2b30bce5
Alejandro Granadillo <slayerfat@gmail.com>
2016-06-30 21:15:48 -0400

    added const to timer spec

--- spec/timer.spec.js
+++ spec/timer.spec.js
@@ -3,20 +3,21 @@ const Timer = require('../src/Timer.js');
 const soundMock = require('./mocks/soundMock');
 const quoteMock = require('./mocks/quoteMock');
 const progressBarMock = require('./mocks/progressBarMock');
-
-const HALF = 18e5;
-const MIN = 6e4;
-// const SEC = 1e3;
+const CONSTANTS = require('../src/constants');
 
 describe('Timer', function () {
   let timer;
+  let options = {
+    sound: soundMock,
+    progress: progressBarMock,
+    end: CONSTANTS.HALF,
+    quote: quoteMock
+  };
   beforeEach(function () {
     jasmine.clock().install();
 
     spyOn(quoteMock, 'random');
     spyOn(progressBarMock, 'tick');
-
-    const options = {sound: soundMock, progress: progressBarMock, end: HALF, quote: quoteMock};
 
     timer = new Timer(options);
   });
@@ -29,6 +30,13 @@ describe('Timer', function () {
     it('should tell the goal is 30 minutes', function () {
       expect(timer.objective).toContain('El objetivo es es de 30 minutos.');
     });
+
+    it('should tell the goal is 5 minutes', function () {
+      options.end = 5 * CONSTANTS.MIN;
+      const anotherTimer = new Timer(options);
+
+      expect(anotherTimer.objective).toContain('El objetivo es es de 5 minutos.');
+    });
   });
 
   describe('should tell me how many seconds have passed', function () {
@@ -37,13 +45,13 @@ describe('Timer', function () {
     });
 
     it('should tell me 1 minute have passed after 1 minute', function () {
-      timer.startTime = MIN;
+      timer.startTime = CONSTANTS.MIN;
       jasmine.clock().tick(6e5);
       expect(timer.msg).toContain('Ha pasado 1 minuto');
     });
 
     it('should tell me 2 minute have passed after 2 minute', function () {
-      timer.startTime = MIN * 2;
+      timer.startTime = CONSTANTS.MIN * 2;
       expect(timer.msg).toContain('Han pasado 2 minutos');
     });
   });
